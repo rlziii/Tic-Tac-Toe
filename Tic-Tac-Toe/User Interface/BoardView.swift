abef4a50963a34e08727615c383482273828ca14
Richard L Zarth III <rlziii@icloud.com>
2020-11-05 21:35:12 -0500

    Rough setup for non-checking Tic-Tac-Toe two-player game (on a single device).

--- Tic-Tac-Toe/User Interface/BoardView.swift
+++ Tic-Tac-Toe/User Interface/BoardView.swift
@@ -1,26 +1,118 @@
 import SwiftUI
 
-struct BoardView: View {
-    @State private var number: Int = 0
+enum PlayerType {
+    case x
+    case o
 
-    private var numberString: String {
-        String(number)
+    var token: String {
+        switch self {
+        case .x:
+            return "❌"
+        case .o:
+            return "⭕️"
+        }
+    }
+}
+
+struct BoardView: View {
+    @State private var currentPlayer: PlayerType = .x
+
+    var body: some View {
+        VStack {
+            Text("Current Player: \(currentPlayer.token)")
+            BoardGridView(rows: 3, columns: 3, size: 100, currentPlayer: $currentPlayer)
+        }
+    }
+}
+
+struct BoardGridView: View {
+    let rows: Int
+    let columns: Int
+    let size: CGFloat
+
+    @Binding var currentPlayer: PlayerType
+
+    private var rowsRange: ClosedRange<Int> {
+        0...(rows - 1)
+    }
+
+    private var columnsRange: ClosedRange<Int> {
+        0...(columns - 1)
     }
 
     var body: some View {
-        Text(numberString)
-            .padding()
-            .onAppear(perform: onAppear)
-    }
-
-    private func onAppear() {
-        _ = Test.addListener(for: "C1yrH44aDDdrQ9JoL3zy") { result in
-            switch result {
-            case let .success(test):
-                self.number = test.number
-            case let .failure(error):
-                print(error.localizedDescription)
+        HStack {
+            ForEach(rowsRange, id: \.self) { a in
+                VStack {
+                    ForEach(columnsRange, id: \.self) { _ in
+                        BoardSpaceView(size: size, currentPlayer: $currentPlayer)
+                            .frame(width: size, height: size)
+                    }
+                }
             }
+        }
+    }
+}
+
+struct BoardSpaceView: View {
+    let size: CGFloat
+
+    @Binding var currentPlayer: PlayerType
+
+    @State private var selection: BoardSpaceSelection = .none
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .size(width: size, height: size)
+
+            BoardSpaceButtonView(size: size, selection: $selection, currentPlayer: $currentPlayer)
+        }
+    }
+}
+
+struct BoardSpaceButtonView: View {
+    let size: CGFloat
+
+    @Binding var selection: BoardSpaceSelection
+    @Binding var currentPlayer: PlayerType
+
+    var body: some View {
+        Button(action: {
+            switch selection {
+            case .x, .o:
+                break // Do nothing
+            case .none:
+                switch currentPlayer {
+                case .x:
+                    selection = .x
+                    currentPlayer = .o
+                case .o:
+                    selection = .o
+                    currentPlayer = .x
+                }
+            }
+        }, label: {
+            Text(selection.token)
+                .font(.largeTitle)
+                .frame(width: size, height: size)
+        })
+    }
+}
+
+enum BoardSpaceSelection {
+    case x
+    case o
+    case none
+
+    var token: String {
+        switch self {
+        case .x:
+            return PlayerType.x.token
+        case .o:
+            return PlayerType.o.token
+        case .none:
+            return ""
         }
     }
 }
